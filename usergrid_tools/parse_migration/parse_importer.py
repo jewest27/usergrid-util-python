89af4249d29aac9fdf27175eb4089e94b487fd3c
Jeff West <jwest@apigee.com>
2016-01-31 18:05:00 -0800

    first version, temporary WIP

---
+++ usergrid_tools/parse_migration/parse_importer.py
@@ -0,0 +1,179 @@
+import json
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+from os import listdir
+import zipfile
+
+# parameters
+from os.path import isfile
+
+import sys
+
+from usergrid import Usergrid
+from usergrid.UsergridClient import UsergridEntity
+
+logger = logging.getLogger('UsergridParseImporter')
+
+parse_id_to_uuid_map = {}
+
+
+def init_logging(stdout_enabled=True):
+    root_logger = logging.getLogger()
+    log_file_name = './usergrid_parse_importer.log'
+    log_formatter = logging.Formatter(fmt='%(asctime)s | %(name)s | %(processName)s | %(levelname)s | %(message)s',
+                                      datefmt='%m/%d/%Y %I:%M:%S %p')
+
+    rotating_file = logging.handlers.RotatingFileHandler(filename=log_file_name,
+                                                         mode='a',
+                                                         maxBytes=2048576000,
+                                                         backupCount=10)
+    rotating_file.setFormatter(log_formatter)
+    rotating_file.setLevel(logging.INFO)
+
+    root_logger.addHandler(rotating_file)
+    root_logger.setLevel(logging.INFO)
+
+    logging.getLogger('boto').setLevel(logging.ERROR)
+    logging.getLogger('urllib3.connectionpool').setLevel(logging.WARN)
+    logging.getLogger('requests.packages.urllib3.connectionpool').setLevel(logging.WARN)
+
+    if stdout_enabled:
+        stdout_logger = logging.StreamHandler(sys.stdout)
+        stdout_logger.setFormatter(log_formatter)
+        stdout_logger.setLevel(logging.INFO)
+        root_logger.addHandler(stdout_logger)
+
+
+def join_roles_to_roles(working_directory):
+    join_file = os.path.join(working_directory, '_Join:roles:_Role.json')
+
+    if os.path.isfile(join_file) and os.path.getsize(join_file) > 0:
+        with open(join_file, 'r') as f:
+            users_to_roles = json.load(f).get('results', [])
+            logger.info('Loaded [%s] Roles->Roles' % len(users_to_roles))
+    else:
+        logger.info('No Roles->Roles to load')
+
+
+def convert_parse_entity(collection, parse_entity):
+    parse_entity['type'] = collection
+
+    if 'name' not in parse_entity and collection.lower() != 'users':
+        parse_entity['name'] = parse_entity['objectId']
+
+    connections = {}
+
+    for name, value in parse_entity.iteritems():
+
+        if isinstance(value, dict):
+            if value.get('__type') == 'Pointer':
+                print 'connection! %s' % value
+                connections[value.get('objectId')] = value.get('className') if value.get('className')[
+                                                                                   0] != '_' else value.get(
+                        'className')[1:]
+
+    return UsergridEntity(parse_entity), connections
+
+
+def load_users_and_roles(working_directory):
+    with open(os.path.join(working_directory, '_User.json'), 'r') as f:
+        users = json.load(f).get('results', [])
+        logger.info('Loaded [%s] Users' % len(users))
+
+    for i, parse_user in enumerate(users):
+        print 'Loading user [%s / %s]' % (i, parse_user['objectId'])
+        usergrid_user, connections = convert_parse_entity('users', parse_user)
+        response = usergrid_user.save()
+
+        if 'uuid' in usergrid_user.entity_data:
+            parse_id_to_uuid_map[parse_user['objectId']] = usergrid_user.get('uuid')
+
+    with open(os.path.join(working_directory, '_Role.json'), 'r') as f:
+        roles = json.load(f).get('results', [])
+        logger.info('Loaded [%s] Roles' % len(roles))
+
+    for i, parse_role in enumerate(roles):
+        print 'Loading role [%s / %s]' % (i, parse_role['objectId'])
+        usergrid_role, connections = convert_parse_entity('roles', parse_role)
+        usergrid_role.save()
+
+        if 'uuid' in usergrid_role.entity_data:
+            parse_id_to_uuid_map[parse_role['objectId']] = usergrid_role.get('uuid')
+
+    join_file = os.path.join(working_directory, '_Join:users:_Role.json')
+
+    if os.path.isfile(join_file) and os.path.getsize(join_file) > 0:
+        with open(join_file, 'r') as f:
+            users_to_roles = json.load(f).get('results', [])
+            logger.info('Loaded [%s] User->Roles' % len(users_to_roles))
+
+    else:
+        logger.info('No Users->Roles to load')
+
+
+def load_entities(working_directory):
+    files = [f for f in listdir(working_directory)
+             if isfile(os.path.join(working_directory, f)) and f[0] != '_']
+
+    for file in files:
+        file_path = os.path.join(working_directory, file)
+        collection = file.split('.')[0]
+        entities = []
+
+        with open(file_path, 'r') as f:
+            entities = json.load(f).get('results')
+
+            print 'Found [%s] entities of type [%s]' % (len(entities), collection)
+
+            for parse_entity in entities:
+                usergrid_entity, connections = convert_parse_entity(collection, parse_entity)
+                response = usergrid_entity.save()
+
+                if response.ok:
+                    print 'Saved Entity: %s' % parse_entity
+
+                    for entity_id, entity_type in connections.iteritems():
+                        uuid = parse_id_to_uuid_map.get(entity_id)
+                        to_entity = UsergridEntity({'uuid': uuid, 'type': entity_type})
+
+                        connect_response = usergrid_entity.connect('pointers', to_entity)
+
+                        if connect_response.ok:
+                            print 'connected'
+                        else:
+                            print connect_response
+
+
+def main():
+    init_logging()
+    Usergrid.init(org_id='jwest1',
+                  app_id='sandbox')
+
+    tmp_dir = '/Users/ApigeeCorporation/tmp'
+    file_path = '/Users/ApigeeCorporation/code/usergrid/usergrid-util-python/usergrid_tools/parse_migration/0e4b82c5-9aad-45de-810a-ff07c281ed2d_1454177649_export.zip'
+
+    file_name = os.path.basename(file_path).split('.')[0]
+
+    working_directory = os.path.join(tmp_dir, file_name)
+
+    with zipfile.ZipFile(file_path, "r") as z:
+        logger.info('Extracting files to directory: %s' % working_directory)
+        z.extractall(working_directory)
+        logger.info('Extraction complete')
+
+    load_users_and_roles(working_directory)
+
+    load_entities(working_directory)
+
+
+# count files
+# iterate internal objects starting with _
+# load users
+# load roles
+# connect roles -> Users
+# connect roles -> roles
+# iterate custom collections
+# end
+
+main()
