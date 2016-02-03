c92ee9677678d417a12a3557efcf540ec6fdd081
Jeff West <jwest@apigee.com>
2016-02-03 10:23:57 -0800

    rev'd version, reformatted

--- setup.py
+++ setup.py
@@ -2,28 +2,29 @@ __author__ = 'Jeff West @ ApigeeCorporation'
 
 from setuptools import setup, find_packages
 
-VERSION = '0.0.2'
+VERSION = '0.0.4'
 
 setup(
-    name='usergrid-tools',
-    version=VERSION,
-    description='Tools for working with Apache Usergrid',
-    url='http://usergrid.apache.org',
-    download_url="https://codeload.github.com/jwest-apigee/usergrid-util-python/zip/v0.0.2",
-    author='Jeff West',
-    author_email='jwest@apigee.com',
-    packages=find_packages(),
-    install_requires=[
-        'requests',
-        'usergrid'
-    ],
-    entry_points={
-        'console_scripts': [
-            'usergrid_data_migrator = usergrid_tools.migration.usergrid_data_migrator:main',
-            'usergrid_index_test = usergrid_tools.indexing.index_test:main',
-            'usergrid_parse_importer = usergrid_tools.parse_importer.parse_importer:main',
-            'usergrid_deleter = usergrid_tools.parse_importer.parse_importer:main',
-            'usergrid_library_check = usergrid_tools.library_check:main',
-        ]
-    }
+        name='usergrid-tools',
+        version=VERSION,
+        description='Tools for working with Apache Usergrid',
+        url='http://usergrid.apache.org',
+        download_url="https://codeload.github.com/jwest-apigee/usergrid-util-python/zip/%s" % VERSION,
+        author='Jeff West',
+        author_email='jwest@apigee.com',
+        packages=find_packages(),
+        install_requires=[
+            'requests',
+            'usergrid',
+            'argparse'
+        ],
+        entry_points={
+            'console_scripts': [
+                'usergrid_data_migrator = usergrid_tools.migration.usergrid_data_migrator:main',
+                'usergrid_index_test = usergrid_tools.indexing.index_test:main',
+                'usergrid_parse_importer = usergrid_tools.parse_importer.parse_importer:main',
+                'usergrid_deleter = usergrid_tools.parse_importer.parse_importer:main',
+                'usergrid_library_check = usergrid_tools.library_check:main',
+            ]
+        }
 )
